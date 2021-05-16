ef9b36788892b3173d021b8387e2d1c0adddebe6
Namaty06 <58531358+Namaty06@users.noreply.github.com>
2021-05-16 08:55:25 +0100

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,16 +23,17 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'adress'=> $this->faker->streetAddress,
-            'city'=> $this->faker->cityPrefix ,
-            'country'=> $this->faker->country,
-            'tel'=> $this->faker->e164PhoneNumber,
-            'zip'=>$this->faker->postcode,
+           'name' => "Yassine Namaty",
+            'email' => "namatyyassine@gmail.com",
+            'adress'=> "hay hassani",
+            'city'=> 'casa' ,
+            'country'=> "maroc",
+            'tel'=> "0619191",
+            'zip'=>20220,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'is_admin'=>true
         ];
     }
 }
